926204e0a4795493ecd8809db5006d5d3b8d2e36
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-06-22 16:37:14 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
+// connect to mongoDB
+const dbURI =
+  "mongodb+srv://Aditya:Aditya1234@blog-website.pu6ze.mongodb.net/blogs-lists?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
-
-app.use((req, res, next) => {
-  console.log("New reqest made:");
-  console.log("host: ", req.hostname);
-  console.log("Path: ", req.path);
-  console.log("Method: ", req.method);
-  next();
-});
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
